8b5eca54843cbf168c7c4dc1c9503763e91e4bae
jean ayen <136095643+Jeanny143@users.noreply.github.com>
2023-11-11 16:55:32 +0800

    add style

--- src/pages/PostsPage.vue
+++ src/pages/PostsPage.vue
@@ -1,19 +1,53 @@
 <template>
     <h1 class="mb-4">Posts</h1>
     <v-textarea label="What's on your mind?"></v-textarea>
-    
+    <div class="posts">
+    <p>{{ postText }}</p>
+    <button @click="incrementLikes">
+      <span>&#10084;</span> {{ likeCount }}
+    </button>
+  </div>
 </template>
 
 
 <script>
-    import { defineComponent } from 'vue';
-
-    export default defineComponent({
-        name: "HomePage",
-    })
+    export default {
+  props: {
+    postText: String,
+    initialLikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      likeCount: this.initialLikes,
+    };
+  },
+  methods: {
+    incrementLikes() {
+      this.likeCount += 1;
+    },
+  },
+};
 </script>
 
 
 <style scoped>
+.post {
+  margin-bottom: 20px;
+  border: 1px solid #ac1212;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+}
 </style>
