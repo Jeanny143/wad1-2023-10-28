5e86fa26a320c5ce9acd6604045e0a83beb7626c
jean ayen <136095643+Jeanny143@users.noreply.github.com>
2023-11-12 14:52:22 +0800

    change style

--- src/pages/PostsPage.vue
+++ src/pages/PostsPage.vue
@@ -1,7 +1,12 @@
-
 <template>
-<h1 class="mb-4">Posts</h1>
-<v-textarea label="What's on your mind?"></v-textarea>
+  <h1 class="mb-4">Posts</h1>
+  <v-textarea label="What's on your mind?"></v-textarea>
+
+  <div>
+    <button @click="addPost">Post</button>
+
+    <Posts v-for="(post, index) in userPosts" :key="index" :postText="post.text" :initialLikes="post.likes" @like="incrementLikes(index)" />
+  </div>
 </template>
 
 <script>
